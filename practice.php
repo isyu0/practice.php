0ffe0c24e9e843e119126c1282f782b7daad802e
”isyu0” <syun.p55@gmail.com>
2019-12-30 03:38:22 +0000

    PHP/Laravel_03

--- practice.php
+++ practice.php
@@ -1,33 +1,49 @@
 <?php
-echo '';
 
-$a=3;$b=7;
-echo $a+$b;
+$name = "ito_shumpei";
 
-$array_month=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-echo $array_month[9];
+if($name == "ito_shumpei"){
+  echo "私は伊藤駿平です。";
+}else{
+  echo "名前が一致しません。";
+}
+echo "\n";
+echo "\n";
 
-$hello="Hello,";
-$name="ito_shumpei";
-$world="'s World!";
-echo $hello.$name.$world;
 
-$tech_boost="tech ";
-$tech_boost.="boost";
-echo $tech_boost;
+$total = 0;
+echo $total;
 
-$calendar_2018 = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" => "6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10月",
-  "November" => "11月",
-  "December" => "  12月",
-];
-echo $calendar_2018["December"];
+for ($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+echo "\n";
+echo "\n";
+
+$fruits = array("apple", "orange", "lemon", "peach", "melon");
+
+foreach($fruits as $fruit){
+  echo $fruit;
+  echo "\n";
+}
+echo "\n";
+
+
+
+/* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する ($i = $start;←エラー発生 $ < $end;←95で終わる)*/
+$end = 100;
+
+for($i = 1; $i <= $end; $i++) {
+
+  // 5で割り切れたら{}内を実行する (echo "\n";で改行)
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
+
+
+
